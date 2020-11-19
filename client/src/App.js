aea7ae6c0f698d3a2f723df18d2f83916aad7a68
Akimberl <mr.burbu.kb@gmail.com>
2020-11-19 18:39:38 +0300

    weather API connected

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,14 @@
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Header from './Components/Header';
 import Footer from './Components/Footer';
+import Weather from './Components/Weather';
 
 function App() {
   return (
     <>
       <Router>
         <Header />
+        <Weather />
         <Switch>
           <Route exact path="/">
             Главная страница
