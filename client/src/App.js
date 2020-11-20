930dffa6774dab15d3bf9505fa191f5473290893
Andrey Belookiy <belookiywhite@icloud.com>
2020-11-20 10:27:08 +0300

    Style

--- client/src/App.js
+++ client/src/App.js
@@ -6,20 +6,15 @@ import Clothes from './Components/Clothes';
 import ContolPanel from './Components/ContolPanel';
 import Weather from './Components/Weather';
 import './index.css'
-import "bootswatch/dist/cosmo/bootstrap.min.css";
-// (examples: darkly, slate, cosmo, spacelab, and superhero. See https://bootswatch.com/ for current theme names.)
-import Weather from './Components/Weather';
-
 
 function App() {
   return (
     <>
       <Router>
         <Header />
-        <Weather />
+        {/* <Weather /> */}
         <Switch>
           <Route exact path="/">
-            <Weather />
             <div className="mainComponent">
             <ContolPanel />
             <Clothes />
