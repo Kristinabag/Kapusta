74245a1e6f7a6adf22a0541be4c8816ff52f544f
Akimberl <mr.burbu.kb@gmail.com>
2020-11-21 20:37:02 +0300

    styles added

--- client/src/Components/ControlPanel/index.js
+++ client/src/Components/ControlPanel/index.js
@@ -1,54 +1,40 @@
 import './style.css';
 import { useDispatch } from 'react-redux';
+import { useState } from 'react';
 import chooseActivity from '../../redux/actions/activity';
 
 function ControlPanel() {
   const dispatch = useDispatch();
+  const [selectValue, setSelectValue] = useState('leasure-walking');
+
+  function setActivity(event) {
+    setSelectValue(event.target.value);
+    dispatch(chooseActivity(selectValue));
+  }
 
   return (
     <div className="ControlPanel">
-      <div className="card border-dark mb-3">
+
+      <div className="card border-primary mb-3">
+        <div className="card-header">Типа активности</div>
         <div className="card-body">
-          <h4 className="card-title">Тип активности: </h4>
-          <select
-            onChange={(e) => dispatch(chooseActivity(e.target.value))}
-            className="nav-link dropdown-toggle"
-            data-toggle="dropdown"
-          >
-            <option className="h-1" value="leasure-walking">
-              Пешая прогулка
-            </option>
-            <option className="m-1" value="chilling-at-home">
-              Отдыхать дома
-            </option>
-            <option className="m-1" value="work-office">
-              Работа в офисе
-            </option>
-            <option className="m-1" value="work-informal">
-              Работа без дресс-кода
-            </option>
-            <option className="m-1" value="leasure-goingout">
-              Встреча с друзьями
-            </option>
-            <option className="m-1" value="sport-jogging">
-              Пробежка
-            </option>
-            <option className="m-1" value="sport-cycling">
-              Велопрогулка
-            </option>
-            <option className="m-1" value="sport-winter">
-              Зимние виды спорта
-            </option>
-            <option className="m-1" value="sport-hiking">
-              Треккинг
-            </option>
+          <select value={selectValue} onChange={setActivity} className="nav-link dropdown-toggle" data-toggle="dropdown">
+            <option className="h-1" value="leasure-walking">Пешая прогулка</option>
+            <option className="m-1" value="chilling-at-home">Отдыхать дома</option>
+            <option className="m-1" value="work-office">Работа в офисе</option>
+            <option className="m-1" value="work-informal">Работа без дресс-кода</option>
+            <option className="m-1" value="leasure-goingout">Встреча с друзьями</option>
+            <option className="m-1" value="sport-jogging">Пробежка</option>
+            <option className="m-1" value="sport-cycling">Велопрогулка</option>
+            <option className="m-1" value="sport-winter">Зимние виды спорта</option>
+            <option className="m-1" value="sport-hiking">Треккинг</option>
           </select>
         </div>
       </div>
 
       <div className="card border-dark mb-3">
+        <div className="card-header">Одежда</div>
         <div className="card-body">
-          <h4 className="card-title">Одежда: </h4>
           <select className="nav-link dropdown-toggle" data-toggle="dropdown">
             <option>Гардероб по умолчанию</option>
             <option>Мой гардероб</option>
@@ -58,17 +44,13 @@ function ControlPanel() {
 
       <div className="recomendation">
         <div className="card border-dark mb-3">
-          <div className="card-header">
-            <h2>Recommedation</h2>
-          </div>
+          <div className="card-header">Рекомендации</div>
           <div className="card-body">
-            <p className="card-text">
-              Сегодня ветрено, поэтому оденься теплее и не забудь зонт, возможны
-              осадки.
-            </p>
+            <p className="card-text">Сегодня ветрено, поэтому оденься теплее и не забудь зонт, возможны осадки.</p>
           </div>
         </div>
       </div>
+
     </div>
   );
 }
--- client/src/Components/Footer/index.js
+++ client/src/Components/Footer/index.js
@@ -1,7 +1,18 @@
+import { Link } from 'react-router-dom';
+import style from './style.module.css';
+
 function Footer() {
   return (
-    <div className="footer">
-      <button type="button" className="btn btn-primary">Primary</button>
+    <div className={`${style.footer}`}>
+      <div>
+        &copy; 2020
+        {' '}
+        <Link to="/elbrus" className={`${style.link}`} target="_blank">elbrusboot.camp</Link>
+        {' '}
+        | By
+        {' '}
+        <Link className={`${style.link}`} to="/gitRepo" target="_blank">Kapusta</Link>
+      </div>
     </div>
   );
 }
